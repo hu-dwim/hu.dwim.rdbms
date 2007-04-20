60350e78f9ebbdeffbcb13a0ca5b4d213742f277
Levente Mészáros <levente.meszaros@gmail.com>
2007-04-20 17:03:01 +0100

    Add format-sql-literal for vectors as bytea under postgresql.

--- postgresql/syntax.lisp
+++ postgresql/syntax.lisp
@@ -22,6 +22,17 @@
         (format-string (princ-to-string (length *binding-entries*))))
       (call-next-method)))
 
+(defmethod format-sql-literal ((literal array) (database postgresql))
+  (format-string "E'")
+  (loop for el across literal
+        do (if (or (<= 0 el 31)
+                   (<= 127 el 255)
+                   (eq el #\')
+                   (eq el #\\))
+               (format *sql-stream* "\\~3,'0o" el)
+               (format-char (code-char el))))
+  (format-string "'::bytea"))
+
 (defmethod format-sql-syntax-node ((variable sql-binding-variable) (database postgresql))
   (push variable *binding-entries*)
   (format-string "$")
