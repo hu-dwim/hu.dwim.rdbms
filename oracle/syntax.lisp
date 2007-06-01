8d3587c1db64972f701fd8199f2e82a4d424fb27
Levente Mészáros <levente.meszaros@gmail.com>
2007-06-01 11:44:17 +0100

    Support binary data as literals for oracle.

--- oracle/syntax.lisp
+++ oracle/syntax.lisp
@@ -4,9 +4,15 @@
 ;;;
 ;;; See LICENCE for details.
 
-(in-package :cl-rdbms.postgresql)
+(in-package :cl-rdbms.oracle)
 
 #.(file-header)
+
+(defmethod format-sql-literal ((literal vector) (database oracle))
+  (format-string "to_Blob('")
+  (loop for el across literal
+        do (format t "~2,'0x" el))
+  (format-string "')"))
 
 (defmethod format-sql-syntax-node ((variable sql-binding-variable) (database oracle))
   (vector-push-extend variable *binding-entries*)
