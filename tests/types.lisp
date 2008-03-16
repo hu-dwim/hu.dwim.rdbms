407e2121e2a1e2d83d0e3d2dd32d68f5e85f4336
Attila Lendvai <attila@lendvai.name>
2008-03-16 20:04:44 +0100

    extend type tests to test the behavior of null

--- tests/types.lisp
+++ tests/types.lisp
@@ -57,26 +57,34 @@
   `(def type-test ,name ,type
        ,@(mapcar
           (lambda (value)
-            (list 'equalp value value))
+            (setf value (ensure-list value))
+            (list 'equalp (first value) (if (rest value)
+                                            (second value)
+                                            (first value))))
           values)))
 
 (def simple-type-test test/types/boolean boolean
+  :null
   t
   nil)
 
 (def simple-type-test test/types/char (char 10)
+  (nil :null)
   "1234567890"
   "áéíóöőúüű ")
 
 (def simple-type-test test/types/varchar (varchar 10)
+  (nil :null)
   "1234567890"
   "áéíóöőúüű")
 
 (def simple-type-test test/types/clob clob
+  (nil :null)
   "1234567890"
   "áéíóöőúüű")
 
 (def simple-type-test test/types/int8 (integer 8)
+  (nil :null)
   0
   1
   -1
@@ -84,6 +92,7 @@
   -128)
 
 (def simple-type-test test/types/int16 (integer 16)
+  (nil :null)
   0
   1
   -1
@@ -91,6 +100,7 @@
   -32768)
 
 (def simple-type-test test/types/int32 (integer 32)
+  (nil :null)
   0
   1
   -1
@@ -98,6 +108,7 @@
   -2147483648)
 
 (def simple-type-test test/types/integer integer
+  (nil :null)
   0
   1
   -1
@@ -105,6 +116,7 @@
   -12345678901234567890123456789012345678)
 
 (def simple-type-test test/types/float float
+  (nil :null)
   0.0
   1.0
   -1.0
@@ -114,9 +126,11 @@
   -1.23e+9)
 
 (def simple-type-test test/types/blob blob
-  (coerce #(1 2 3 4 5 6 7 8 9 0) '(vector (unsigned-byte 8))))
+  (nil :null)
+  #.(coerce #(1 2 3 4 5 6 7 8 9 0) '(vector (unsigned-byte 8))))
 
 (def type-test test/types/date date
+  (eq nil :null)
   ;; TODO (signals 'error (local-time:now))
   (local-time:local-time= (local-time:parse-datestring "1000-01-01"))
   (local-time:local-time= (local-time:parse-datestring "0000-01-01"))
@@ -124,6 +138,7 @@
   (local-time:local-time= (local-time:parse-datestring "3000-01-01")))
 
 (def type-test test/types/time time
+  (eq nil :null)
   ;; TODO (signals 'error (local-time:parse-timestring "06:06:06+02:00"))
   (local-time:local-time= (local-time:parse-timestring "06:06:06Z"))
   (local-time:local-time= (local-time:parse-timestring "00:00:00Z"))
@@ -131,10 +146,12 @@
 
 ;; TODO take tz into account when comparing
 (def type-test test/types/timestamp (timestamp #f)
+  (eq nil :null)
   ;; TODO (signals 'error (local-time:parse-timestring "2006-06-06T06:06:06+02:00"))
   (local-time:local-time= (local-time:parse-timestring "2006-06-06T06:06:06Z")))
 
 (def type-test test/types/timestamp-tz (timestamp #t)
+  (eq nil :null)
   (local-time:local-time= (local-time:parse-timestring "2006-06-06T06:06:06Z"))
   (local-time:local-time= (local-time:parse-timestring "2006-06-06T06:06:06-01:00"))
   (local-time:local-time= (local-time:parse-timestring "2006-06-06T06:06:06-01:30"))
