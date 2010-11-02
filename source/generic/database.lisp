de5e744f941d5ea1f0bb730198566b0af095d948
Attila Lendvai <attila@lendvai.name>
2010-11-02 14:59:08 +0100

    a better version of calculate-rdbms-name-with-utf-8-length-limit as a comment for now

--- source/generic/database.lisp
+++ source/generic/database.lisp
@@ -94,6 +94,57 @@
           (setf name-as-string (string+ name-as-string hash))))
       name-as-string)))
 
+#+nil ; TODO this version is much better, but it's not backwards compatible, so keep it commented out for now...
+(def function calculate-rdbms-name-with-utf-8-length-limit (name length-limit &key prefix (illegal-characters "+*\\/-~%")
+                                                                 (hash-separator "_"))
+  "When needed, drops characters from the middle of names and replaces them with a hash code of the uncut name."
+  (check-type name string)
+  (assert (every [or (lower-case-p !1) (digit-char-p !1) (not (alphanumericp !1))] name) () "RDBMS names must always be lower case, because some backends ignore the case: ~S" name)
+  (flet ((compress (vector ratio)
+           "Compress byte vectors by xor'ing the dropped elements to the remaining elements."
+           (check-type ratio positive-integer)
+           (check-type vector (vector (unsigned-byte 8) *))
+           (bind ((new-length (/ (length vector) ratio))
+                  (new-vector (make-array new-length :element-type '(unsigned-byte 8))))
+             (check-type new-length positive-integer)
+             (loop
+               :for index :from 0 :below new-length
+               :do (loop
+                     :for factor :from 1 :below ratio
+                     :for dropped-index = (+ index (* factor new-length))
+                     :do (setf (aref new-vector index) (logxor (aref vector index)
+                                                               (aref vector dropped-index)))))
+             new-vector)))
+    (bind ((name-as-string (string+ prefix name)))
+      (iter (for char :in-sequence illegal-characters)
+            (nsubstitute #\_ char name-as-string :test #'char=))
+      (bind ((name-as-bytes (string-to-octets name-as-string :encoding :utf-8)))
+        (when (> (length name-as-bytes)
+                 length-limit)
+          ;; drop the resolution of upper/lower case when calculating the hash
+          (bind ((hash (compress (ironclad:digest-sequence :sha256 (string-to-octets name :encoding :utf-8)) 8))
+                 (hash-as-string (ironclad:byte-array-to-hex-string hash))
+                 (hash-length (length hash-as-string))
+                 (chopped-name name-as-string)
+                 (extra-character-count (* 2 (length hash-separator))))
+            (assert (<= (+ hash-length extra-character-count) length-limit))
+            ;; drop chars one-by-one because we have no idea about their UTF-8 length
+            (iter (while (> (length name-as-bytes)
+                            (- length-limit hash-length)))
+                  (for middle-index = (floor (/ (length chopped-name) 2)))
+                  (setf chopped-name (string+ (subseq chopped-name 0 middle-index)
+                                              (subseq chopped-name (1+ middle-index))))
+                  (setf name-as-bytes (string-to-octets chopped-name :encoding :utf-8)))
+            (bind ((part-size (floor (/ (- (length chopped-name) extra-character-count) 2))))
+              (setf name-as-string (string+ (subseq chopped-name 0 part-size)
+                                            hash-separator
+                                            hash-as-string
+                                            hash-separator
+                                            (subseq chopped-name (+ extra-character-count part-size))))))
+          (assert (= (length name-as-string) length-limit))))
+      (assert (<= (length name-as-string) length-limit))
+      name-as-string)))
+
 (def (function e) enable-ddl-query-cache (database)
   (setf (ddl-query-cache-of database) (make-hash-table)))
 
