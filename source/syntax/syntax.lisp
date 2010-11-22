d9e2d792c420052f4b5d554dafc0c4aef7be8f6e
Tomas Hlavaty <tomas.hlavaty@knowledgetools.de>
2010-11-22 16:02:25 +0100

    leftover force-unquoting removed from rdbms

--- source/syntax/syntax.lisp
+++ source/syntax/syntax.lisp
@@ -46,9 +46,6 @@
     :type sql-type)
    (suppress-unquoting
     :type boolean
-    :initform nil)
-   (force-unquoting
-    :type boolean
     :initform nil))
   (:documentation "Represents an SQL literal.")
   (:format-sql-syntax-node
